5a53be5a5bb22227265bb25e2e0eb610f976a466
DriesVanherpe1 <dries.vanherpe@student.howest.be>
2017-09-26 09:28:21 +0200

    commit

---
+++ lab_01/oefeningen basis week-01.py
@@ -0,0 +1,33 @@
+# labo_intro oef1 (steeds commentarieeren)
+#naam = "dries"
+#achternaam = "vanherpe"
+#print("hello world,",naam,achternaam)
+
+basis= float(input("wat is basis?"))
+hoogte =float(input("wat is de hoogte?"))
+opp = (basis*hoogte)/2
+print("de totale oppervlakte van de driehoek is {0:.2f}".format(opp))
+# # ^oefening vijf bereken de oppervlakte
+
+# dagen = int(input("geef het aantal dagen op:"))
+# uren = int(input("geef he aantal uren op :"))
+# minuten = int(input("geef het aantal minuten op:"))
+# seconden = int(input("geef het aantal seconden op:"))
+# seconden = ((dagen*24)*3600) + (uren*3600) + (minuten*60)+ seconden
+# print("het aantal seconden bedraagt : {0}".format(seconden))
+
+# seconden = int(input("geef het aantal seconden op:"))
+# minuten,seconden = divmod(seconden,60)
+# uren, minuten= divmod(minuten,60)
+# dagen, uren = divmod(uren,24)
+# print("{0}:{1}:{2}:{3}".format(dagen,uren,minuten,seconden))
+
+# getal= int(input("geef een getal op: "))
+# uitkomst = getal + (getal**2) + (getal**3)
+# print("de uitkomst is {0}".format(uitkomst))
+
+# get1 = int(input("geef het eerste getal in: "))
+# get2 = int(input("geef het tweede getal in :"))
+# uitkomst = (get1+get2)**2
+# print("de uitkomst is {0}".format(uitkomst))
+
---
+++ lab_01/oefeningen uitbr week-01.py
@@ -0,0 +1,37 @@
+# import math
+# print(dir(math))
+
+# from math import pi
+# getal = float(input("geef een getal in : "))
+# uitkomst = getal * pi
+# print("Na uw getal te vermenigvuldigen met PI is dit je uitkomst : {0}".format
+
+# import sys
+# print(dir(sys))
+
+# from sys import version
+# print("u gebruikt deze python-versie:\n{0}".format(version))
+
+# import datetime
+# print(dir(datetime))
+
+# from datetime import datetime
+# print("dit is de huidige tijd:\n{0}".format(datetime.now()))
+
+# from math import pi
+# straal = float(input("geef een straal in : "))
+# opp = (straal**2)*pi
+# print("de oppervlakte van de cirkel met straal {0} is {1:.2f}.".format(straal,opp))
+
+# from math import pi
+# straal = float(input("geef een straal in : "))
+# vol = (4/3)*pi*(straal**3)
+# print("het volume van de bol met straal {0} is {1:.2f}".format(straal,vol)
+
+# import calendar
+# maand = int(input("geef een maandnummer in :"))
+# jaar = int(input("geef een jaartal in :"))
+# print("dit is de kalender van de maand {0} uit het jaar {1}\n {2}".format(maand,jaar,calendar.month(jaar,maand)))
+
+# import multiprocessing
+# print("je hebt {0} CPU's.".format(multiprocessing.cpu_count()))
